7ff64fddf973d93991dcf9238f635694e4a77d20
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-05-14 05:48:49 -0700

    Added servlet to handle request to update task details

---
+++ src/main/java/com/taskmanagementsystem/controllers/taskServlet/TaskDeleteServlet.java
@@ -0,0 +1,20 @@
+package com.taskmanagementsystem.controllers.taskServlet;
+
+import com.taskmanagementsystem.dao.TaskDAO;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/delete-task")
+public class TaskDeleteServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        new TaskDAO().deleteTask(id);
+        response.sendRedirect("tasks");
+    }
+}
+
